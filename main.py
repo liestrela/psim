048b234c89d4a6340c7131b3554764141dd980ba
liestrela <estrela@riseup.net>
2024-12-01 14:06:24 -0300

    Refatorando parcialmente

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 #!/bin/env python3
-from psim.window import Window
+from pool.window import Window
 import sys
 
-W = 1000;
+W = 1300;
 H = 600;
-DEFAULT_N  = 5;
-DEFAULT_MR = 50;
-
-USAGE_MSG = f"""usage: {sys.argv[0]} [n] [max_radius]
-n - number of particles
-max_radius - max radius of particle
------------------------------------
-press 'p' to pause the simulation
-left click to add a particle""";
 
 if __name__ == "__main__":
-	if len(sys.argv)>1:
-		if sys.argv[1]=="-h":
-			print(USAGE_MSG);
-			exit();
-
-	n_par = int(sys.argv[1]) if len(sys.argv)>1 else DEFAULT_N;
-	max_radius = int(sys.argv[2]) if len(sys.argv)>2 else DEFAULT_MR;
-
-	win = Window("Particle Sandbox", W, H, n_par, max_radius);
+	win = Window("Sinuca", W, H);
 
 	win.bgcolor = (0x0, 0x28, 0x3c);
 	tela = "menu"
